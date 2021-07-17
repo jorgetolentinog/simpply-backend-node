da43565c623d07861258865d5c3f9350fcf409e8
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2021-07-16 21:25:01 -0500

    -

--- src/repository/service/list.js
+++ src/repository/service/list.js
@@ -1,7 +1,14 @@
 function ServiceRepositoryList({ airtableAPI }) {
   return async () => {
     const resp = await airtableAPI.get("service?view=Grid%20view");
-    return resp.data.records;
+    return resp.data.records.map((o) => ({
+      id: o.id,
+      pk: o.fields.pk,
+      name: o.fields.name,
+      description: o.fields.description,
+      price: o.fields.price,
+      createdTime: o.createdTime,
+    }));
   };
 }
 
--- src/service/service/list.js
+++ src/service/service/list.js
@@ -1,14 +1,7 @@
 function ServiceServiceList({ serviceRepositoryList }) {
   return async () => {
     const result = await serviceRepositoryList();
-    return result.map((o) => ({
-      id: o.id,
-      pk: o.fields.pk,
-      name: o.fields.name,
-      description: o.fields.description,
-      price: o.fields.price,
-      createdTime: o.createdTime,
-    }));
+    return result;
   };
 }
 
