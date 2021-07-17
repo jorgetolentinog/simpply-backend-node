ba6eecf23bd377eb093e7d48e3ce2a45b7447a25
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2021-07-17 00:04:58 -0500

    -

--- src/infrastructure/airtable/api.js
+++ src/infrastructure/airtable/api.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { debug } = require("../logger/debug")("airtable:api");
+const { axiosRequestLogger, axiosResponseLogger } = require("../logger/http");
 
 const airtableAPI = axios.create({
   baseURL: "https://api.airtable.com/v0/appgUUt9aJtuOxtnD/",
@@ -10,21 +10,7 @@ const airtableAPI = axios.create({
   },
 });
 
-airtableAPI.interceptors.request.use(function (config) {
-  const method = config.method.toUpperCase();
-  const url = `${config.baseURL}${config.url}`;
-  const data = (config.data && JSON.stringify(config.data)) || "";
-  debug(`[request ] ${method} ${url} ${data}`);
-  return config;
-});
-
-airtableAPI.interceptors.response.use(function (response) {
-  const method = response.request.method;
-  const url = `${response.config.baseURL}${response.config.url}`;
-  const status = response.status;
-  const data = (response.data && JSON.stringify(response.data)) || "";
-  debug(`[response] ${method} ${url} ${status} ${data}`);
-  return response;
-});
+airtableAPI.interceptors.request.use(axiosRequestLogger);
+airtableAPI.interceptors.response.use(axiosResponseLogger);
 
 module.exports = { airtableAPI };
