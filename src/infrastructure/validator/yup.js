d45adf55231237da86078993de28f5f15f4c36df
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2021-07-19 14:55:07 -0500

    -

--- src/infrastructure/validator/yup.js
+++ src/infrastructure/validator/yup.js
@@ -1,5 +1,4 @@
 const yup = require("yup");
-const dayjs = require("dayjs");
 
 yup.addMethod(yup.string, "isodate", function () {
   const pattern = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
