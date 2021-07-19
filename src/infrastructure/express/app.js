7be8f8894b6f61c20898c7c7d2d170217f0b5fe2
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2021-07-19 15:44:48 -0500

    -

--- src/infrastructure/express/app.js
+++ src/infrastructure/express/app.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { json: bodyParserJSON } = require("body-parser");
-const {
-  default: graphqlIDE,
-} = require("graphql-playground-middleware-express");
 const { Schema } = require("../graphql/schema");
 
 const app = express();
 app.disable("x-powered-by");
 app.use(bodyParserJSON());
 
-app.get(
-  "/api/graphql",
-  graphqlIDE({
-    endpoint: "/api/graphql",
-    settings: {
-      "schema.polling.enable": false,
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: Schema,
+    graphiql: {
+      headerEditorEnabled: true,
     },
   })
 );
-
-app.post("/api/graphql", graphqlHTTP({ schema: Schema }));
 
 module.exports = { app };
