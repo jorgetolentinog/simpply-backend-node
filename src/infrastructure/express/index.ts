3425702af79f29a495155a9a8ea7d02c3095684b
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2021-07-22 18:16:40 -0500

    -

--- src/infrastructure/express/index.ts
+++ src/infrastructure/express/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import { GraphQLError } from "graphql";
 import { graphqlHTTP } from "express-graphql";
 import { json as bodyParserJSON } from "body-parser";
 import { Schema } from "../graphql/schema";
+import { ZodError } from "zod";
 
 const app = express();
 app.disable("x-powered-by");
@@ -13,6 +15,29 @@ app.use(
     schema: Schema,
     graphiql: {
       headerEditorEnabled: true,
+    },
+    customFormatErrorFn: (err: GraphQLError) => {
+      let detail = undefined;
+      let message = err.message;
+      let stacktrace: string | undefined = err.stack;
+
+      if (err.originalError instanceof ZodError) {
+        const issue = err.originalError.issues[0];
+        const issueAt = issue.path.join(".");
+        message = `Validation Error: ${issue.message} at ${issueAt}`;
+        stacktrace = undefined;
+        detail = err.originalError.issues;
+      }
+
+      return {
+        message,
+        extensions: {
+          detail,
+          stacktrace,
+        },
+        path: err.path,
+        locations: err.locations,
+      };
     },
   })
 );
